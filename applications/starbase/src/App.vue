d8ade59e5a49ad0a29cfe37f9f3caf7b4331db25
s-kiips <ranjitkarsudeen14@gmail.com>
2017-09-28 22:23:22 +0545

    new application jokester added

--- applications/starbase/src/App.vue
+++ applications/starbase/src/App.vue
@@ -5,7 +5,12 @@
     </div>
 </template>
 
+
+
 <script>
+
+
+
     export default {
         name: 'app',
         data() {
@@ -15,13 +20,32 @@
         },
         methods:{
             fetchCharacter(){
-                fetch('http://swapi.co/api/people',{
+                fetch('http://swapi.co/api/people/1',{
                     method:'GET',
                     mode: 'no-cors',
                 })
-                    .then(response => response.json())
+//                    .then(response => response.json())
+//                    .then(json => console.log(json));
+                    .then(
+                        function (response) {
+                            console.log(response);
+                            try {
+                                JSON.parse(JSON.stringify(json));
+                                console.log("valid part",json);
+                                console.log("valid");
+                            } catch (e) {
+                                console.log("invalid");
+                                // correct the invalid json here
+//                                JSON.parse(JSON.stringify(json));
+//                                console.log("invalid part",response)
+                            }
+                        },
+                        function (error) {
+                            console.log('error');
+            })
                     .then(json => console.log(json))
             }
+
         }
     }
 </script>
