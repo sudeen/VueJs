811f404d3cba54664990bd81b2037e77c2ea0eb5
s-kiips <ranjitkarsudeen14@gmail.com>
2017-09-29 00:29:52 +0545

    card feature added for jokes

--- applications/jokester/src/components/App.vue
+++ applications/jokester/src/components/App.vue
@@ -2,19 +2,30 @@
     <div id="app">
         <h3>Got Jokes?</h3>
         <button class="btn btn-primary" @click="initJokes">Add Ten Random Jokes</button>
-        <div class="">
-            {{$store.state}}
+        <br>
+        <br>
+        <div class="col-md-12">
+            <joke
+            v-for="(joke, index) in $store.state.jokes"
+            :joke="joke"
+            :key="index"
+            ></joke>
         </div>
+
 
     </div>
 </template>
 
 <script>
     import {mapActions} from 'vuex'
+    import Joke from './Joke.vue'
 
     export default {
         methods: mapActions([
             'initJokes'
-        ])
+        ]),
+        components:{
+            Joke
+        }
     }
 </script>
