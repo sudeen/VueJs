fcc4bb0e82a59f37d7d88781ac786e8c26f57e30
s-kiips <ranjitkarsudeen14@gmail.com>
2017-09-29 01:01:49 +0545

    completed jokester app with filter funtionality

--- applications/jokester/src/components/App.vue
+++ applications/jokester/src/components/App.vue
@@ -4,10 +4,19 @@
         <button class="btn btn-primary" @click="initJokes">Add Ten Random Jokes</button>
         <button class="btn btn-primary" @click="addJoke">Add a Joke</button>
         <br>
+        <span v-for="type in types">
+            <input
+                    type="checkbox"
+                    :value="type"
+                    v-model="checkedTypes"
+                    checked/>
+            <label>{{type}}</label>&nbsp;
+        </span>
         <br>
         <div class="col-md-12">
             <joke
             v-for="(joke, index) in $store.state.jokes"
+            v-show="checkedTypes.includes(joke.type)"
             :joke="joke"
             :index="index"
             :key="index"
@@ -23,6 +32,12 @@
     import Joke from './Joke.vue'
 
     export default {
+        data(){
+            return {
+                types:['general','knock-knock','programming'],
+                checkedTypes:['general','knock-knock','programming']
+            }
+        },
         methods: mapActions([
             'initJokes',
             'addJoke'
