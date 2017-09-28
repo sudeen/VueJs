ab973c3e713bee0cc693c710e483246823a04613
s-kiips <ranjitkarsudeen14@gmail.com>
2017-09-28 10:32:42 +0545

    removed mispelled folder added new one

---
+++ applications/starbase/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <h3>{{title}}</h3>
+        <button class="btn btn-primary" @click="fetchCharacter">Test fetch character</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        data() {
+            return {
+                title: 'Generate your Team'
+            }
+        },
+        methods:{
+            fetchCharacter(){
+                fetch('http://swapi.co/api/people',{
+                    method:'GET',
+                    mode: 'no-cors',
+                })
+                    .then(response => response.json())
+                    .then(json => console.log(json))
+            }
+        }
+    }
+</script>
